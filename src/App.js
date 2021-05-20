c53442043679ab107230cacda343d519a7f9cc62
musakanneh <mkanneh17@alustudent.com>
2021-05-20 22:00:10 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -20,29 +20,43 @@ function App() {
       title: "Summer engineering analyst",
       amount: 30000,
       date: new Date(2021, 6, 20)
+    },
+    {
+      id: 'e4',
+      title: "Tarinhophyta",
+      amount: 20.67,
+      date: new Date(2021, 10, 20)
     }
   ]
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>My Expense Tracker</h2>
+
       <EspenseItmem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].date}>
-      </EspenseItmem>
+        date={expenses[0].date}
+      />
 
       <EspenseItmem
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[1].date}>
-      </EspenseItmem>
+        date={expenses[1].date}
+      />
 
       <EspenseItmem
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date}>
-      </EspenseItmem>
+        date={expenses[2].date}
+      />
+
+      <EspenseItmem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+      
     </div>
   );
 }
