63f88674233d753d70fd07c5aa0955baaa056ec3
musakanneh <mkanneh17@alustudent.com>
2021-05-24 07:41:53 +0200

    Added expense filter

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-// import ExpenseFilter from "./components/NewExpense/ExpenseFilter";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet paper",
+    amount: 300.67,
+    date: new Date(2021, 5, 19),
+  },
+  {
+    id: "e2",
+    title: "Car insurence",
+    amount: 3000,
+    date: new Date(2020, 5, 17),
+  },
+  {
+    id: "e3",
+    title: "Summer engineering analyst",
+    amount: 30000,
+    date: new Date(2021, 6, 20),
+  },
+  {
+    id: "e4",
+    title: "Tarinhophyta",
+    amount: 20.67,
+    date: new Date(2021, 10, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet paper",
-      amount: 300.67,
-      date: new Date(2021, 5, 19),
-    },
-    {
-      id: "e2",
-      title: "Car insurence",
-      amount: 3000,
-      date: new Date(2020, 5, 17),
-    },
-    {
-      id: "e3",
-      title: "Summer engineering analyst",
-      amount: 30000,
-      date: new Date(2021, 6, 20),
-    },
-    {
-      id: "e4",
-      title: "Tarinhophyta",
-      amount: 20.67,
-      date: new Date(2021, 10, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
